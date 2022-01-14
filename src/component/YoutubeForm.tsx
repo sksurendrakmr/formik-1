66faed1dd01805a986a45794b859bbaf8c75a45c
sksurendrakmr <sksurendrakmr@gmail.com>
2022-01-14 19:03:37 +0530

    3- useFormik Hook

--- src/component/YoutubeForm.tsx
+++ src/component/YoutubeForm.tsx
@@ -1,13 +1,32 @@
 import React from "react";
 import "../App.css";
 
+import { useFormik } from "formik";
+
 /**
  * In this component we create a form and
  * manage the form state, handling form submission
  * and validation and display error message using formik
  */
 
+/**
+ * Step1 : Import a hook called useFormik which the library provides.
+ *
+ *
+ *         This useFormik hooks takes in an object as its parameter and
+ *         this hook then return an object which contains a variety of
+ *         useful properties and methods that we can use with our form.
+ *
+ *         The object that we get as a return value is going to help us
+ *         with :-
+ *         1) Managing the form state
+ *         2) Handling form submission
+ *         3) Validation and error messages.
+ */
+
 const YoutubeForm = () => {
+  const formik = useFormik({});
+
   return (
     <>
       <form>
