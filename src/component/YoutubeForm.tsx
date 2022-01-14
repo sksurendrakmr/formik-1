50d13d235a4e00e81f348bd896549a37ff1631bd
sksurendrakmr <sksurendrakmr@gmail.com>
2022-01-14 19:21:13 +0530

    Managing Form state

--- src/component/YoutubeForm.tsx
+++ src/component/YoutubeForm.tsx
@@ -22,22 +22,64 @@ import { useFormik } from "formik";
  *         1) Managing the form state
  *         2) Handling form submission
  *         3) Validation and error messages.
+ *
+ *      The object that useFormik takes as an arguments contains
+ *      1) initialValues properties which is an object and it contains
+ *         the initial values for all our form fields.
+ *      2) The properties for initialValues (in our case name, email and channel)
+ *         corresponds to the name attribute of the individual fields.
+ *
+ *
+ * Step 2: We need to add the onChange and the value prop for each of the
+ *         form fields. This is require to ensure the form fields are tracked in
+ *         react by formik.
  */
 
 const YoutubeForm = () => {
-  const formik = useFormik({});
+  const { handleChange, values } = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      channel: "",
+    },
+    onSubmit: () => {},
+  });
+
+  /**
+   * values ==> An Object provided by formik. This object always reflects
+   *            the state of the form.
+   * handleChange => It's a formik helper to update the values object.
+   */
 
   return (
     <>
       <form>
         <label htmlFor="name">Name</label>
-        <input type="text" id="name" name="name" />
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={values.name}
+          onChange={handleChange}
+        />
 
         <label htmlFor="email">Email</label>
-        <input type="email" id="email" name="email" />
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={values.email}
+          onChange={handleChange}
+        />
 
         <label htmlFor="channel">Channel</label>
-        <input type="text" id="channel" name="channel" />
+        <input
+          type="text"
+          id="channel"
+          name="channel"
+          value={values.channel}
+          onChange={handleChange}
+        />
 
         <button>Submit</button>
       </form>
